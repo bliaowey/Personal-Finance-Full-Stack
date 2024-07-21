709793b1dcf07d8265c28f9c909f2f7cad31bda6
Brian Liao <bliao29772522@gmail.com>
2024-07-21 14:02:07 -0400

    first commit

---
+++ finance-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ListAccountRecordComponent from './components/ListAccountRecordComponent'
+
+function App() {
+
+  return (
+    <>
+      <ListAccountRecordComponent />
+    </>
+  )
+}
+
+export default App
