da7a8a193d48f9e2155433d16859a87a5b5eab99
Brian Liao <bliao29772522@gmail.com>
2024-08-13 16:02:48 -0400

    implement backend for uploading and reading a file

--- finance-backend/src/main/java/com/example/financebackend/FinanceBackendApplication.java
+++ finance-backend/src/main/java/com/example/financebackend/FinanceBackendApplication.java
@@ -1,13 +1,25 @@
 package com.example.financebackend;
 
+import com.example.financebackend.service.FileStorageService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
-public class FinanceBackendApplication {
+public class FinanceBackendApplication implements CommandLineRunner{
+
+	FileStorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceBackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		storageService.init();
+	}
 }
---
+++ finance-backend/src/main/java/com/example/financebackend/entity/FileInfo.java
@@ -0,0 +1,16 @@
+package com.example.financebackend.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FileInfo {
+    private String name;
+    private String url;
+
+    public FileInfo(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+}
