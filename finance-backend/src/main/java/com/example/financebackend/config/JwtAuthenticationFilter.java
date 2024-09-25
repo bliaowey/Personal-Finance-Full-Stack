ef8203589c201ef4e2572857eb1bdafb36f320dc
Brian Liao <bliao29772522@gmail.com>
2024-09-25 14:56:25 -0400

    Added the some JWT security

---
+++ finance-backend/src/main/java/com/example/financebackend/config/JwtAuthenticationFilter.java
@@ -0,0 +1,57 @@
+package com.example.financebackend.config;
+
+import com.example.financebackend.service.JwtService;
+import com.example.financebackend.service.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.util.StringUtil;
+import org.aspectj.weaver.patterns.IToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserService userService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String autHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+
+        if(StringUtils.isEmpty(autHeader) || !org.apache.commons.lang3.StringUtils.startsWith(autHeader, "bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = autHeader.substring(7);
+        userEmail = jwtService.extractUserName(jwt);
+
+        if(io.micrometer.common.util.StringUtils.isNotEmpty(userEmail) && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userService.userDetailsService().loadUserByUsername(userEmail);
+            if(jwtService.isTokenValid(jwt, userDetails)) {
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+
+                UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+                token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                securityContext.setAuthentication(token);
+                SecurityContextHolder.setContext(securityContext);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
